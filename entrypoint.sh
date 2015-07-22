04e157e1715f844d12d8a9e82d6c0d5f1b68fd81
Sameer Naik <sameer@damagehead.com>
2015-07-22 22:39:40 +0530

    entrypoint.sh: allow users to specify arguments to `nginx`

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,9 +15,18 @@ mkdir -p ${NGINX_SITECONF_DIR}
 chmod -R 755 ${NGINX_SITECONF_DIR}
 chown -R root:root ${NGINX_SITECONF_DIR}
 
+# allow arguments to be passed to nginx
+if [[ ${1:0:1} = '-' ]]; then
+  EXTRA_ARGS="$@"
+  set --
+elif [[ ${1} == nginx || ${1} == $(which nginx) ]]; then
+  EXTRA_ARGS="${@:2}"
+  set --
+fi
+
 # default behaviour is to launch nginx
 if [[ -z ${1} ]]; then
-  exec $(which nginx) -c /etc/nginx/nginx.conf -g "daemon off;"
+  exec $(which nginx) -c /etc/nginx/nginx.conf -g "daemon off;" ${EXTRA_ARGS}
 else
   exec "$@"
 fi
