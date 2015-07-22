c02ceec4773ae9b3726d2c5e2587d1272e5c0188
Sameer Naik <sameer@damagehead.com>
2015-07-22 22:36:42 +0530

    entrypoint.sh: create `NGINX_SITECONF_DIR` directory with the right permissions

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,4 +10,9 @@ chown -R ${NGINX_USER}:root ${NGINX_LOG_DIR}
 mkdir -p ${NGINX_TEMP_DIR}
 chown -R root:root ${NGINX_TEMP_DIR}
 
+# create site config dir
+mkdir -p ${NGINX_SITECONF_DIR}
+chmod -R 755 ${NGINX_SITECONF_DIR}
+chown -R root:root ${NGINX_SITECONF_DIR}
+
 exec /usr/sbin/nginx -c /etc/nginx/nginx.conf -g "daemon off;"
