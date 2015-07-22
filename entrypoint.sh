f0bc39582707c5734ee3a48cb027c444d5becf9a
Sameer Naik <sameer@damagehead.com>
2015-07-22 15:05:47 +0530

    nginx.conf: set the ngx_pagespeed `FileCachePath` to `/var/lib/nginx/ngx_pagespeed`

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 set -e
 
-# fix permissions and ownership of /var/lib/nginx
-mkdir -p -m 755 /var/lib/nginx
+# create temp dir
+mkdir -p /var/lib/nginx
 chown -R root:root /var/lib/nginx
-
-# fix permissions and ownership of /var/cache/ngx_pagespeed
-mkdir -p -m 755 /var/cache/ngx_pagespeed
-chown -R www-data:www-data /var/cache/ngx_pagespeed
 
 exec /usr/sbin/nginx -c /etc/nginx/nginx.conf -g "daemon off;"
