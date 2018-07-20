e60974ac77a63ecba1bf185256075202c7acb674
Sameer Naik <sameer@damagehead.com>
2018-07-20 17:42:52 +0530

    optimize image build with multistage builds

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,13 @@
-FROM ubuntu:bionic-20180526
-
-LABEL maintainer="sameer@damagehead.com"
+FROM ubuntu:bionic-20180526 AS builder
 
 ENV RTMP_VERSION=1.2.1 \
     NPS_VERSION=1.11.33.4 \
+    FDK_AAC_VERSION=0.1.6 \
+    X264_VERSION=snapshot-20161228-2245-stable \
     LIBAV_VERSION=12.2 \
     NGINX_VERSION=1.12.2 \
-    NGINX_USER=www-data \
-    NGINX_SITECONF_DIR=/etc/nginx/sites-enabled \
-    NGINX_LOG_DIR=/var/log/nginx \
-    NGINX_TEMP_DIR=/var/lib/nginx \
-    NGINX_BUILD_DIR=/var/cache/nginx
-
-ARG BUILD_LIBAV=false
+    NGINX_BUILD_ASSETS_DIR=/etc/docker-nginx/build \
+    NGINX_BUILD_ROOT_DIR=/etc/docker-nginx/build/rootfs
 
 ARG WITH_DEBUG=false
 
@@ -20,9 +15,31 @@ ARG WITH_PAGESPEED=true
 
 ARG WITH_RTMP=true
 
-COPY assets/build/ ${NGINX_BUILD_DIR}/
+RUN apt-get update \
+ && DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends -y \
+      wget ca-certificates make gcc g++ pkg-config
 
-RUN bash ${NGINX_BUILD_DIR}/install.sh
+COPY assets/build/ ${NGINX_BUILD_ASSETS_DIR}/
+
+RUN chmod +x ${NGINX_BUILD_ASSETS_DIR}/install.sh
+
+RUN ${NGINX_BUILD_ASSETS_DIR}/install.sh
+
+FROM ubuntu:bionic-20180526
+
+LABEL maintainer="sameer@damagehead.com"
+
+ENV NGINX_USER=www-data \
+    NGINX_SITECONF_DIR=/etc/nginx/sites-enabled \
+    NGINX_LOG_DIR=/var/log/nginx \
+    NGINX_TEMP_DIR=/var/lib/nginx
+
+RUN apt-get update \
+ && DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends -y \
+      libssl1.1 libxslt1.1 libgd3 libgeoip1 libfdk-aac1 \
+ && rm -rf /var/lib/apt/lists/*
+
+COPY --from=builder /etc/docker-nginx/build/rootfs /
 
 COPY assets/config/nginx.conf /etc/nginx/nginx.conf
 
--- assets/build/install.sh
+++ assets/build/install.sh
@@ -1,66 +1,62 @@
 #!/bin/bash
 set -e
 
-NGINX_DOWNLOAD_URL="http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz"
-NGINX_RTMP_MODULE_DOWNLOAD_URL="https://github.com/arut/nginx-rtmp-module/archive/v${RTMP_VERSION}.tar.gz"
-NGX_PAGESPEED_DOWNLOAD_URL="https://github.com/pagespeed/ngx_pagespeed/archive/v${NPS_VERSION}-beta.tar.gz"
-PSOL_DOWNLOAD_URL="https://dl.google.com/dl/page-speed/psol/${NPS_VERSION}.tar.gz"
-LIBAV_DOWNLOAD_URL="https://libav.org/releases/libav-${LIBAV_VERSION}.tar.gz"
-
-RUNTIME_DEPENDENCIES="libssl1.0.0 libxslt1.1 libpcre++ libgd3 libxpm4 libgeoip1"
-BUILD_DEPENDENCIES="wget ca-certificates make gcc g++ libssl-dev libxslt-dev libpcre++-dev libgd-dev libgeoip-dev"
+install_packages() {
+  DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends "$@"
+}
 
 download_and_extract() {
   src=${1}
   dest=${2}
   tarball=$(basename ${src})
 
-  if [ ! -f ${NGINX_BUILD_DIR}/${tarball} ]; then
-    echo "Downloading ${tarball}..."
-    mkdir -p ${NGINX_BUILD_DIR}/
-    wget ${src} -O ${NGINX_BUILD_DIR}/${tarball}
+  if [[ ! -f ${NGINX_BUILD_ASSETS_DIR}/${tarball} ]]; then
+    echo "Downloading ${1}..."
+    wget ${src} -O ${NGINX_BUILD_ASSETS_DIR}/${tarball}
   fi
 
   echo "Extracting ${tarball}..."
   mkdir ${dest}
-  tar -zxf ${NGINX_BUILD_DIR}/${tarball} --strip=1 -C ${dest}
-  rm -rf ${NGINX_BUILD_DIR}/${tarball}
+  tar xf ${NGINX_BUILD_ASSETS_DIR}/${tarball} --strip=1 -C ${dest}
+}
+
+strip_debug() {
+  local dir=${1}
+  local filter=${2}
+  for f in $(find "${dir}" -name "${filter}")
+  do
+    if [[ -f ${f} ]]; then
+      strip --strip-all ${f}
+    fi
+  done
 }
 
 ${WITH_RTMP} && {
-  ${BUILD_LIBAV} && {
-    RUNTIME_DEPENDENCIES="$RUNTIME_DEPENDENCIES libfdk-aac1 libx264-152"
-    BUILD_DEPENDENCIES="$BUILD_DEPENDENCIES yasm libfdk-aac-dev libx264-dev"
-  } || {
-    RUNTIME_DEPENDENCIES="$RUNTIME_DEPENDENCIES ffmpeg"
-  }
-}
+  # download_and_extract "https://sourceforge.net/projects/opencore-amr/files/fdk-aac/fdk-aac-${FDK_AAC_VERSION}.tar.gz" "${NGINX_BUILD_ASSETS_DIR}/fdk-aac"
+  download_and_extract "http://prdownloads.sourceforge.net/opencore-amr/fdk-aac-${FDK_AAC_VERSION}.tar.gz" "${NGINX_BUILD_ASSETS_DIR}/fdk-aac"
+  cd ${NGINX_BUILD_ASSETS_DIR}/fdk-aac
+  ./configure \
+    --prefix=/usr \
+    --enable-shared \
+    --disable-static \
+    --disable-example
+  make -j$(nproc)
+  make install
+  make DESTDIR=${NGINX_BUILD_ROOT_DIR} install
 
-apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends -y ${RUNTIME_DEPENDENCIES} ${BUILD_DEPENDENCIES}
+  install_packages yasm
+  download_and_extract "http://ftp.videolan.org/pub/x264/snapshots/x264-${X264_VERSION}.tar.bz2" "${NGINX_BUILD_ASSETS_DIR}/x264"
+  cd ${NGINX_BUILD_ASSETS_DIR}/x264
+  ./configure \
+    --prefix=/usr \
+    --enable-shared \
+    --disable-opencl
+  make -j$(nproc)
+  make install
+  make DESTDIR=${NGINX_BUILD_ROOT_DIR} install
 
-# enable debug support
-${WITH_DEBUG} && {
-  EXTRA_ARGS="${EXTRA_ARGS} --with-debug"
-}
-
-# prepare rtmp module support
-${WITH_RTMP} && {
-  EXTRA_ARGS="${EXTRA_ARGS} --add-module=${NGINX_BUILD_DIR}/nginx-rtmp-module"
-  download_and_extract "${NGINX_RTMP_MODULE_DOWNLOAD_URL}" "${NGINX_BUILD_DIR}/nginx-rtmp-module"
-}
-
-# prepare pagespeed module support
-${WITH_PAGESPEED} && {
-  EXTRA_ARGS="${EXTRA_ARGS} --add-module=${NGINX_BUILD_DIR}/ngx_pagespeed"
-  download_and_extract "${NGX_PAGESPEED_DOWNLOAD_URL}" "${NGINX_BUILD_DIR}/ngx_pagespeed"
-  download_and_extract "${PSOL_DOWNLOAD_URL}" "${NGINX_BUILD_DIR}/ngx_pagespeed/psol"
-}
-
-# build libav
-${WITH_RTMP} && ${BUILD_LIBAV} && {
-  download_and_extract "${LIBAV_DOWNLOAD_URL}" "${NGINX_BUILD_DIR}/libav"
-  cd ${NGINX_BUILD_DIR}/libav
+  download_and_extract "https://libav.org/releases/libav-${LIBAV_VERSION}.tar.gz" "${NGINX_BUILD_ASSETS_DIR}/libav"
+  cd ${NGINX_BUILD_ASSETS_DIR}/libav
   ./configure \
     --prefix=/usr \
     --disable-debug \
@@ -70,57 +66,69 @@ ${WITH_RTMP} && ${BUILD_LIBAV} && {
     --enable-gpl \
     --enable-libx264 \
     --enable-libfdk-aac
-  make -j$(nproc) && make install
+  make -j$(nproc)
+  make DESTDIR=${NGINX_BUILD_ROOT_DIR} install
+
+  download_and_extract "https://github.com/arut/nginx-rtmp-module/archive/v${RTMP_VERSION}.tar.gz" ${NGINX_BUILD_ASSETS_DIR}/nginx-rtmp-module
+  EXTRA_ARGS+=" --add-module=${NGINX_BUILD_ASSETS_DIR}/nginx-rtmp-module"
 }
 
-# build nginx with modules enabled at build time
-download_and_extract "${NGINX_DOWNLOAD_URL}" "${NGINX_BUILD_DIR}/nginx"
-cd ${NGINX_BUILD_DIR}/nginx
+${WITH_PAGESPEED} && {
+  download_and_extract "https://github.com/apache/incubator-pagespeed-ngx/archive/v${NPS_VERSION}-beta.tar.gz" ${NGINX_BUILD_ASSETS_DIR}/ngx_pagespeed
+  download_and_extract "https://dl.google.com/dl/page-speed/psol/${NPS_VERSION}.tar.gz" ${NGINX_BUILD_ASSETS_DIR}/ngx_pagespeed/psol
+  EXTRA_ARGS+=" --add-module=${NGINX_BUILD_ASSETS_DIR}/ngx_pagespeed"
+}
+
+download_and_extract "http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz" ${NGINX_BUILD_ASSETS_DIR}/nginx
+cd ${NGINX_BUILD_ASSETS_DIR}/nginx
+install_packages libpcre++-dev libssl-dev zlib1g-dev libxslt1-dev libgd-dev libgeoip-dev
 ./configure \
   --prefix=/usr/share/nginx \
+  --sbin-path=/usr/sbin/nginx \
   --conf-path=/etc/nginx/nginx.conf \
-  --sbin-path=/usr/sbin \
-  --http-log-path=/var/log/nginx/access.log \
   --error-log-path=/var/log/nginx/error.log \
-  --lock-path=/var/lock/nginx.lock \
   --pid-path=/run/nginx.pid \
-  --http-client-body-temp-path=${NGINX_TEMP_DIR}/body \
-  --http-fastcgi-temp-path=${NGINX_TEMP_DIR}/fastcgi \
-  --http-proxy-temp-path=${NGINX_TEMP_DIR}/proxy \
-  --http-scgi-temp-path=${NGINX_TEMP_DIR}/scgi \
-  --http-uwsgi-temp-path=${NGINX_TEMP_DIR}/uwsgi \
-  --with-pcre-jit \
-  --with-ipv6 \
+  --lock-path=/var/lock/nginx.lock \
+  --with-threads \
   --with-http_ssl_module \
-  --with-http_stub_status_module \
+  --with-http_v2_module \
   --with-http_realip_module \
-  --with-http_auth_request_module \
   --with-http_addition_module \
+  --with-http_xslt_module \
+  --with-http_image_filter_module \
+  --with-http_sub_module \
   --with-http_dav_module \
-  --with-http_geoip_module \
   --with-http_gunzip_module \
   --with-http_gzip_static_module \
-  --with-http_image_filter_module \
-  --with-http_v2_module \
-  --with-http_sub_module \
-  --with-http_xslt_module \
-  --with-stream \
-  --with-stream_ssl_module \
+  --with-http_auth_request_module \
+  --with-http_stub_status_module \
+  --with-http_geoip_module \
+  --http-log-path=/var/log/nginx/access.log \
+  --http-client-body-temp-path=/var/lib/nginx/body \
+  --http-proxy-temp-path=/var/lib/nginx/proxy \
+  --http-fastcgi-temp-path=/var/lib/nginx/fastcgi \
+  --http-uwsgi-temp-path=/var/lib/nginx/uwsgi \
+  --http-scgi-temp-path=/var/lib/nginx/scgi \
   --with-mail \
   --with-mail_ssl_module \
-  --with-threads \
+  --with-stream \
+  --with-stream_ssl_module \
+  --with-pcre-jit \
+  --with-cc-opt='-O2 -fstack-protector-strong -Wformat -Werror=format-security -fPIC -D_FORTIFY_SOURCE=2' \
+  --with-ld-opt='-Wl,-Bsymbolic-functions -Wl,-z,relro -Wl,-z,now -fPIC' \
   ${EXTRA_ARGS}
 
-make -j$(nproc) && make install
+make -j$(nproc)
+make DESTDIR=${NGINX_BUILD_ROOT_DIR} install
 
 # copy rtmp stats template
 ${WITH_RTMP} && {
-  cp ${NGINX_BUILD_DIR}/nginx-rtmp-module/stat.xsl /usr/share/nginx/html/
+  cp ${NGINX_BUILD_ASSETS_DIR}/nginx-rtmp-module/stat.xsl ${NGINX_BUILD_ROOT_DIR}/usr/share/nginx/html/
 }
 
 # create default configuration
-mkdir -p /etc/nginx/sites-enabled
-cat > /etc/nginx/sites-enabled/default <<EOF
+mkdir -p ${NGINX_BUILD_ROOT_DIR}/etc/nginx/sites-enabled
+cat > ${NGINX_BUILD_ROOT_DIR}/etc/nginx/sites-enabled/default <<EOF
 server {
   listen 80 default_server;
   listen [::]:80 default_server ipv6only=on;
@@ -153,7 +161,11 @@ server {
 }
 EOF
 
-# cleanup
-apt-get purge -y --auto-remove ${BUILD_DEPENDENCIES}
-rm -rf ${NGINX_BUILD_DIR}/{nginx,nginx-rtmp-module,ngx_pagespeed,libav}
-rm -rf /var/lib/apt/lists/*
+strip_debug "${NGINX_BUILD_ROOT_DIR}/usr/bin/" "*"
+strip_debug "${NGINX_BUILD_ROOT_DIR}/usr/sbin/" "*"
+strip_debug "${NGINX_BUILD_ROOT_DIR}/usr/lib/" "*.so"
+strip_debug "${NGINX_BUILD_ROOT_DIR}/usr/lib/" "*.so.*"
+
+rm -rf ${NGINX_BUILD_ROOT_DIR}/usr/share/man
+rm -rf ${NGINX_BUILD_ROOT_DIR}/usr/share/include
+rm -rf ${NGINX_BUILD_ROOT_DIR}/usr/lib/pkgconfig
